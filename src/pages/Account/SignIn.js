ea92cb889c341c73376e8ac6859768ccb8868f78
suriya21ITR109 <124364442+suriya21ITR109@users.noreply.github.com>
2023-11-21 14:15:34 +0530

    Update SignIn.js

--- src/pages/Account/SignIn.js
+++ src/pages/Account/SignIn.js
@@ -28,7 +28,7 @@ function SigninForm() {
     setIsLoading(true); // Show loading popup
 
     try {
-      const response = await axios.post('http://localhost:5000/api/signin', formData);
+      const response = await axios.post('https://thecandid.onrender.com/api/signin', formData);
 
       if (response.status === 200) {
         setSuccessMessage('Sign-in successful');
